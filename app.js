a1ec263c803e9b4d57e5378e1f9fdc1ae9647544
Jofre Moreno <jofremoreno@gmail.com>
2019-08-13 19:09:56 +0200

    ADD error 404 page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 const app = express();
 const port = 3000;
+app.use(express.static("public")); //para que cargue la carpeta public
 
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/views/index.html"))
@@ -16,6 +17,9 @@ app.get("/portfolio", (req, res) =>
   res.sendFile(path.join(__dirname, "/views/portfolio.html"))
 );
 
-app.use(express.static("public")); //para que cargue la carpeta public
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/404.html"));
+});
+
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
